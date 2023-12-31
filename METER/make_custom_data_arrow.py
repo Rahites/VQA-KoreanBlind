1b76366460fa67a06ff632d04b81e02825296a2e
Rahites <hitesarang@gmail.com>
2023-12-31 18:24:22 +0900

    Add METER

---
+++ METER/make_custom_data_arrow.py
@@ -0,0 +1,5 @@
+from meter.utils.write_coco_korean import make_arrow
+root = '<Pretrain Data Path>'
+arrows_root = '<Save Arrow Data Path>'
+
+make_arrow(root, arrows_root)
---
+++ METER/meter/datamodules/__init__.py
@@ -0,0 +1,25 @@
+from .vg_caption_datamodule import VisualGenomeCaptionDataModule
+from .f30k_caption_karpathy_datamodule import F30KCaptionKarpathyDataModule
+from .coco_caption_karpathy_datamodule import CocoCaptionKarpathyDataModule
+from .conceptual_caption_datamodule import ConceptualCaptionDataModule
+from .sbu_datamodule import SBUCaptionDataModule
+from .vqav2_datamodule import VQAv2DataModule
+from .nlvr2_datamodule import NLVR2DataModule
+from .snli_datamodule import SNLIDataModule
+
+from .coco_caption_korean_datamodule import CocoCaptionKoreanDataModule
+from .vqav2_datamodule_v1 import VQAv2DataModule_v1
+
+_datamodules = {
+    "vg": VisualGenomeCaptionDataModule,
+    "f30k": F30KCaptionKarpathyDataModule,
+    "coco": CocoCaptionKarpathyDataModule,
+    "gcc": ConceptualCaptionDataModule,
+    "sbu": SBUCaptionDataModule,
+    "vqa": VQAv2DataModule,
+    "nlvr2": NLVR2DataModule,
+    "snli": SNLIDataModule,
+    
+    "coco_korean": CocoCaptionKoreanDataModule,
+    "vqa_v1": VQAv2DataModule_v1
+}
